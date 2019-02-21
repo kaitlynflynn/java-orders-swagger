08823952b537621ac72e564bd10c1c98fb552827
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2019-02-21 12:38:16 -0800

    Initial proj setup; added deps

---
+++ src/main/java/com/lambdaschool/ordersswagger/OrdersSwaggerApplication.java
@@ -0,0 +1,15 @@
+package com.lambdaschool.ordersswagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdersSwaggerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(OrdersSwaggerApplication.class, args);
+    }
+
+}
